de13f9d9d9c7032cbd7f6d409e952e17e9729fd7
Sam Berry <sberry925@gmail.com>
2018-08-27 09:09:41 +0000

    Add thread collector for unique thread testing

--- src/main/kotlin/org/samberry/uniquethreads/UniqueThreadsResource.kt
+++ src/main/kotlin/org/samberry/uniquethreads/UniqueThreadsResource.kt
@@ -1,16 +1,33 @@
 package org.samberry.uniquethreads
 
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.ConcurrentHashMap
 
 @RequestMapping("/unique-threads")
 @RestController
 class UniqueThreadsResource {
+    private var threads = ConcurrentHashMap<Long, String>()
+
     @ResponseBody
     @GetMapping("/current-thread-id")
     fun fetchThreadId(): Long {
-        return Thread.currentThread().id
+        val threadId = Thread.currentThread().id
+        threads[threadId] = ""
+        return threadId
+    }
+
+    @ResponseBody
+    @GetMapping
+    fun fetchUniqueThreads(): Set<Long> {
+        return threads.keys().toList().toSet()
+    }
+
+    @DeleteMapping
+    fun deleteUniqueThreads() {
+        threads.clear()
     }
 }
