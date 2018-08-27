43526eebce19395932f685c5add02cd863cbbde8
Sam Berry <sberry925@gmail.com>
2018-08-27 07:32:33 +0000

    Add leading slash to path
    For consistency.

--- src/main/kotlin/org/samberry/intermediatevalue/IntermediateValueResource.kt
+++ src/main/kotlin/org/samberry/intermediatevalue/IntermediateValueResource.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
 
-@RequestMapping("intermediate-values")
+@RequestMapping("/intermediate-values")
 @RestController
 class IntermediateValueResource(
     private val intermediateValueService: IntermediateValueService
